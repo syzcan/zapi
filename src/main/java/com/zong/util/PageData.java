167432818e8d51de34c988f44538dc9c662074ec
suyz <suyz@192.168.222.128>
2017-04-11 14:44:41 +0800

    PageData

--- src/main/java/com/zong/util/PageData.java
+++ src/main/java/com/zong/util/PageData.java
@@ -1,30 +1,28 @@
 package com.zong.util;
 
 import java.io.Serializable;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
 /**
- * 说明：参数封装Map，通过request对象将请求参数进行封装
+ * @desc 说明：参数封装Map，通过request对象将请求参数进行封装，作为页面参数传递和查询参数传递
+ * @author zong
+ * @date 2016年3月23日
  */
 @SuppressWarnings("rawtypes")
 public class PageData extends HashMap implements Map, Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	Map map = null;
 	HttpServletRequest request;
 
 	@SuppressWarnings("unchecked")
 	public PageData(HttpServletRequest request) {
 		this.request = request;
 		Map properties = request.getParameterMap();
-		Map returnMap = new HashMap();
 		Iterator entries = properties.entrySet().iterator();
 		Map.Entry entry;
 		String name = "";
@@ -44,29 +42,26 @@ public class PageData extends HashMap implements Map, Serializable {
 			} else {
 				value = valueObj.toString();
 			}
-			returnMap.put(name, value);
+			put(name, value);
 		}
-		map = returnMap;
 	}
 
 	public PageData() {
-		map = new HashMap();
 	}
 
 	@SuppressWarnings("unchecked")
 	public PageData(Object key, Object value) {
-		map = new HashMap();
-		map.put(key, value);
+		super.put(key, value);
 	}
 
 	@Override
 	public Object get(Object key) {
 		Object obj = null;
-		if (map.get(key) instanceof Object[]) {
-			Object[] arr = (Object[]) map.get(key);
+		if (super.get(key) instanceof Object[]) {
+			Object[] arr = (Object[]) super.get(key);
 			obj = request == null ? arr : (request.getParameter((String) key) == null ? arr : arr[0]);
 		} else {
-			obj = map.get(key);
+			obj = super.get(key);
 		}
 		return obj;
 	}
@@ -76,60 +71,9 @@ public class PageData extends HashMap implements Map, Serializable {
 	}
 
 	@SuppressWarnings("unchecked")
-	@Override
 	public PageData put(Object key, Object value) {
-		map.put(key, value);
+		super.put(key, value);
 		return this;
-	}
-
-	@Override
-	public Object remove(Object key) {
-		return map.remove(key);
-	}
-
-	public void clear() {
-		map.clear();
-	}
-
-	public boolean containsKey(Object key) {
-		// TODO Auto-generated method stub
-		return map.containsKey(key);
-	}
-
-	public boolean containsValue(Object value) {
-		// TODO Auto-generated method stub
-		return map.containsValue(value);
-	}
-
-	public Set entrySet() {
-		// TODO Auto-generated method stub
-		return map.entrySet();
-	}
-
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return map.isEmpty();
-	}
-
-	public Set keySet() {
-		// TODO Auto-generated method stub
-		return map.keySet();
-	}
-
-	@SuppressWarnings("unchecked")
-	public void putAll(Map t) {
-		// TODO Auto-generated method stub
-		map.putAll(t);
-	}
-
-	public int size() {
-		// TODO Auto-generated method stub
-		return map.size();
-	}
-
-	public Collection values() {
-		// TODO Auto-generated method stub
-		return map.values();
 	}
 
 }
